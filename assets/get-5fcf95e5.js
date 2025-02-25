88a47d4f30ab2792b7059068a259ade7b92f483d
underoot <underoot@users.noreply.github.com>
2025-02-25 22:05:29 +0000

    deploy: 086109fc5ec20816857b82714b547fdfb51ea659

---
+++ assets/get-5fcf95e5.js
@@ -0,0 +1,1 @@
+import{g as t,a as o,b as m,c as i}from"./lib-3b8b2064.js";const a=["up","right","down","left"],e=t();o(e,{selector:"#game"});m(e);setInterval(()=>{i(e,a[Math.floor(Math.random()*a.length)])},1e3);
